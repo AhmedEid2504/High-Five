e22f0a6ef50b8f89e38c0b53915c0ff9d30afd72
Ahmed Eid <ahmedeid2504@gmail.com>
2023-02-01 17:15:14 +0200

    adds last section for landing page with buy link to store

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const open = document.getElementById ('open');
 const close = document.getElementById ('close');
 const active = document.querySelector ('.active');
 
-
-
-    open.addEventListener('click', () => {
-        if (open.classList.contains('fa-bars')) {
-            open.classList.remove('fa-bars');
-            open.classList.add('fa-chevron-up');
-            active.classList.add('show-nav')
-        } else if (open.classList.contains('fa-chevron-up')) {
-            open.classList.remove('fa-chevron-up');
-            open.classList.add('fa-bars');
-            active.classList.remove('show-nav')
-        }
-    })
+open.addEventListener('click', () => {
+    if (open.classList.contains('fa-bars')) {
+        open.classList.remove('fa-bars');
+        open.classList.add('fa-chevron-up');
+        active.classList.add('show-nav')
+    } else if (open.classList.contains('fa-chevron-up')) {
+        open.classList.remove('fa-chevron-up');
+        open.classList.add('fa-bars');
+        active.classList.remove('show-nav')
+    }
+})
 
     
 
