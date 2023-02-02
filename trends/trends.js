ab84c7e4cf90154bb6b854e28bdf7bfe1ebe7560
Ahmed Eid <ahmedeid2504@gmail.com>
2023-02-03 01:31:51 +0200

    adds finished slider responsive only needs data for other slides also adds jumping hover text for trend name

--- trends/trends.js
+++ trends/trends.js
@@ -16,13 +16,22 @@ open.addEventListener('click', () => {
 
 
 //trends 
-var i = 0;
-var trends = [];
-var time = 3000;
+const buttons = document.querySelectorAll('[data-carousel-button]');
 
-//trends list 
-trends[0] = '.sunmain'
-    
+buttons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
+
+        const activeSlides = slides.querySelector("[data-active")
+        let newIndex = [...slides.children].indexOf(activeSlides)+offset;
+        if(newIndex < 0) newIndex = slides.children.length-1
+        if(newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlides.dataset.active
+    })
+})
 
 
 
